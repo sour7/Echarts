46ed93e6132b65e783ae93d52a005816e251be99
Sourabh Kumar <hellosourabh040@gmail.com>
2023-04-27 12:53:29 +0530

    "bar-graph updated"

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,35 +1,38 @@
-import React from 'react'
+import React from 'react';
 import ReactECharts from 'echarts-for-react';
+
 const BarChart = ({ data }) => {
-    const options = {
-       
+
+    // Get unique alcohol values
+    const xAxisData = [...new Set(data.map(item => item.Alcohol))];
+
+    // Get minimum magnesium value for each alcohol category
+    const yAxisData = xAxisData.map(alcohol => Math.min(...data.filter(item => item.Alcohol === alcohol).map(item => item.Magnesium)));
+
+    const option = {
         xAxis: {
             type: 'category',
-            data: data.map((d) => d.Alcohol),
+            data: xAxisData,
             name: 'Alcohol'
         },
         yAxis: {
             type: 'value',
-            name: 'Magnesium'
+            name: 'Min Magnesium'
         },
         series: [
             {
-                data: data.map((d) => d.Magnesium),
-                type: 'bar',
-                smooth: true,
-            },
-        ],
-        tooltip: {
-            trigger: 'axis',
-        },
+                data: yAxisData,
+                type: 'bar'
+            }
+        ]
     };
 
     return (
-        <div>
-            <h2>Bar Chart</h2>
-            <ReactECharts option={options} />
+        <div style={{overflow:'auto'}}>
+            <h2>Bar Graph</h2>
+            <ReactECharts echarts={echarts} option={option} />
         </div>
     );
-}
+};
 
-export default BarChart
+export default BarChart;
